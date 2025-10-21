f500c1e58700932eed80ce4fbc96d9e788ce39f7
HanByul Yang <hanbyul.yang@gmail.com>
2025-10-21 17:39:28 +0900

    update vtk-compile-tools

--- ports/vtk-compile-tools/portfile.cmake
+++ ports/vtk-compile-tools/portfile.cmake
@@ -2,13 +2,13 @@ set(VCPKG_BUILD_TYPE release)  # tools
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 set(VCPKG_POLICY_MISMATCHED_NUMBER_OF_BINARIES enabled)
 
-set(SHORT_VERSION 9.3)
+set(SHORT_VERSION 9.4)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Kitware/VTK
-    REF 09a76bc55b37caad94d0d8ebe865caaed1b438af # v9.3.x used by ParaView 5.12.0
-    SHA512 396ee901fafacae8aef860b9c9c17cb92ae8b4969527fd271ad8dd9f6a9e0dc8e3dc807c8d43cc585608ad101a64edcd7aff49e1580c7a61a817c2ea8e2655f5
+    REF 13acb1a5dd0ad7f7635f2511f44e599733643d06
+    SHA512 46e22b5f21cba95f75d89f2cea3a2157437fd06bacc07e1fb35575894c35099d35b629f3caf8ebed7fa872dfa2282a59d976ec56a9dcfbbf0162806c30a9df2e
     HEAD_REF master
     PATCHES
         name-suffix.diff
@@ -29,7 +29,7 @@ vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/vtkcompiletools-${SHORT_VERSION})
 vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/${PORT}/vtkcompiletools-config-version.cmake" "set(PACKAGE_VERSION_UNSUITABLE TRUE)" "# allow host tools on any arch")
 
-vcpkg_copy_tools(AUTO_CLEAN TOOL_NAMES vtkParseJava-${SHORT_VERSION} vtkWrapHierarchy-${SHORT_VERSION} vtkWrapJava-${SHORT_VERSION} vtkWrapPython-${SHORT_VERSION} vtkWrapPythonInit-${SHORT_VERSION})
+vcpkg_copy_tools(AUTO_CLEAN TOOL_NAMES vtkParseJava-${SHORT_VERSION} vtkWrapHierarchy-${SHORT_VERSION} vtkWrapJava-${SHORT_VERSION} vtkWrapPython-${SHORT_VERSION} vtkWrapPythonInit-${SHORT_VERSION} vtkWrapSerDes-${SHORT_VERSION})
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/doc")
 
